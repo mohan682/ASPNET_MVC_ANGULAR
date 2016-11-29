38bb3ad556212b8750bb7b43da2c004628dde7fb
mbesta <murali.besta@aegon.co.uk>
2016-11-29 12:08:10 +0000

    PracticeDemo Project

---
+++ PracticeDemo/PracticeDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PracticeDemo.Startup))]
+namespace PracticeDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
