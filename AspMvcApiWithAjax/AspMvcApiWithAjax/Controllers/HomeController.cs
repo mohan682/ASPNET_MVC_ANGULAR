69e91c92fba7fe9697a9ed44308c5c60c74f7ee4
mbesta <murali.besta@aegon.co.uk>
2016-12-02 09:32:13 +0000

    updated with ajax project

--- AspMvcApiWithAjax/AspMvcApiWithAjax/Controllers/HomeController.cs
+++ AspMvcApiWithAjax/AspMvcApiWithAjax/Controllers/HomeController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AspMvcApiWithAjax.Controllers
 {
     public class HomeController : Controller
     {
+        FormsAuthenticationTicket obj = new FormsAuthenticationTicket();
         public ActionResult Index()
         {
+           
+
             ViewBag.Title = "Home Page";
 
             return View();
